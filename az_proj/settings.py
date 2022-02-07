ae75550ec90c85e4dbe60c9dced3c6898215a64c
gaurav <iamvaruag@gmail.com>
2022-02-07 16:16:35 +0530

    'fft3'

--- az_proj/settings.py
+++ az_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!mdqps)a4gy5%ny0u81)^n%gxm#b@fgyuavvmdl08_(reky3hr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
